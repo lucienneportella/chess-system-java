9e1666ac142b52947ceb45db5ec11d1b7e7d5db3
Lucienne Portella <giustilucienne@gmail.com>
2022-04-22 09:39:23 -0300

    Possible moves of a piece

--- src/chess/pieces/Rock.java
+++ src/chess/pieces/Rock.java
@@ -14,4 +14,10 @@ public class Rock extends ChessPiece{
 		return "R";
 	}
 
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		return mat;
+	}
+
 }
